afb4aaff8eb8d34833c2f361935feeb41dfda302
shyamvadaliya <shyamvadaliya25@gmail.com>
2026-06-15 13:39:27 +0530

    Create yfinance_test.py

---
+++ yfinance_test.py
@@ -0,0 +1,34 @@
+import os
+import requests
+import yfinance as yf
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+stock = "RELIANCE.NS"
+
+df = yf.download(
+    stock,
+    period="5d",
+    progress=False,
+    auto_adjust=True
+)
+
+close_price = round(float(df["Close"].iloc[-1]), 2)
+
+message = f"""
+✅ YFinance Working
+
+Stock: RELIANCE
+Current Price: ₹{close_price}
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
